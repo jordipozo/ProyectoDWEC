ee5b1a8f9fd96bd81cbdfb0c13bf96d7b47bb63a
jordipozo <jpozoc@gmail.com>
2019-10-14 21:41:41 +0200

    cierre

--- Objetos/objetosFuncionCall1.js
+++ Objetos/objetosFuncionCall1.js
@@ -1,6 +1,9 @@
 var person = {
   fullName: function() {
     return this.firstName + " " + this.lastName;
+  },
+  fullname2: function(){
+    return this.lastName + " " +this.firstName;
   }
 }
 var person1 = {
@@ -13,3 +16,4 @@ var person2 = {
 }
 person.fullName.call(person1);  // Muestra "John Doe". Aquí el objeto que actua como this es person1
 person.fullName.call(person2);  // Muestra "Mary Doe". En este caso, quién actua como this es person2
+person.fullname2.call(person2);
