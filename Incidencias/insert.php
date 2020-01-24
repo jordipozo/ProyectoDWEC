7daf6a7440e71b2d0488f571c9f43415a03243b2
jordipozo <jpozoc@gmail.com>
2020-01-24 21:27:07 +0100

    cambios insert correctos 240120

--- Incidencias/insert.php
+++ Incidencias/insert.php
@@ -2,10 +2,10 @@
 require 'connection.php';
 // Campos de la tabla incomingIncidents
 $codeT=1;
-$desc=$_POST['troubleDescr'];
+$desc=$_POST['desc'];
 $userId=1;
 //$assTeamId=sha1($_POST['password']);
-//$tskDate=$_POST['nombre'];
+$tskDate=$_POST['fecha'];
 //$assClassId=$_POST['apellido'];
 //$solved=$_POST['usuario'];
 //$current=$_POST['password'];
@@ -13,7 +13,7 @@ $userId=1;
 
 //Insert
 $sql="INSERT into tbl_incomingincidents (codeTask,description,idUser,idAssignedTeam,taskDate,assignDate,idAssociadedtClass,isSolved,isCurrent,solvedDate,closeDate)
-        values ('$codeT','$desc','$userId',NULL,NOW(),NULL,NULL,0,1,NULL,NULL)";
+        values ('$codeT','$desc','$userId',NULL,'$tskDate',NULL,NULL,0,1,NULL,NULL)";
 echo $sql;
 sleep (10); 
 echo mysqli_query($con,$sql);
--- Incidencias/queries.js
+++ Incidencias/queries.js
@@ -1,17 +1,23 @@
 function insertarRegistro() {
-    var fecha = $("#fecha").val();
+
+    var elemento = $("#fecha").val().split("/");
+    console.log('elemento: ' + elemento);
+    var fecha = elemento[2] + '/' + elemento[1] + '/' + elemento[0];
+    //var fecha = $("#fecha").val();
+    alert(fecha);
     var usuario = $("#username").val();
     var ubicacion = $("#classroomName").val();
-    var descripcion = $("#troubleDesc").val();
+    var descripcion = $("#troubleDescr").val();
+    console.log(descripcion);
     $("#respuesta").html("Por favor espera un momento");
     $.ajax({
         type: "POST",
-        dataType: "text",
+        dataType: "html",
         url: "insert.php",
         data: "fecha=" + fecha + "&usuario=" + usuario + "&aula=" + ubicacion + "&desc=" + descripcion,
         success: function(resp) {
             $('#respuesta').html(resp);
-            console.log(data);
+
             //Limpiar();
             //Cargar();
         }
